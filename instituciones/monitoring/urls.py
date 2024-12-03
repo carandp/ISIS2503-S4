46775036a0cac7d89c71da628bd70332dce0b1db
Carlos <c.penaa@uniandes.edu.co>
2024-12-03 01:53:29 -0500

    instituciones fix?

--- instituciones/monitoring/urls.py
+++ instituciones/monitoring/urls.py
@@ -20,7 +20,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('/instituciones/', views.index, name='index'),
-    path('', include('facturas.urls')),
+    path('', include('instituciones.urls')),
     path(r'', include('django.contrib.auth.urls')),
     path(r'', include('social_django.urls')),
 
