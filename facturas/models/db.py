7303e45176e8439f7cac9ae9337f86752ff6ceea
Carlos <c.penaa@uniandes.edu.co>
2024-12-02 20:54:32 -0500

    facturas db fix

--- facturas/models/db.py
+++ facturas/models/db.py
@@ -6,11 +6,11 @@ client = motor.motor_asyncio.AsyncIOMotorClient(
     "mongodb://facturas_user:isis2503@10.128.0.83:27017?retryWrites=true&w=majority"
 )
 db = client.get_database("facturas_db")
-places_collection = db.get_collection("facturas")
+facturas_collection = db.get_collection("facturas")
 
 async def set_places_db():
     # Creates a unique index on the code field
-    await places_collection.create_index("code", unique=True)
+    await facturas_collection.create_index("code", unique=True)
 
 # Represents an ObjectId field in the database.
 PyObjectId = Annotated[str, BeforeValidator(str)]
