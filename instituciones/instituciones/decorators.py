6711d3b0eb6444b67181d5cde97750f6dcb90bb6
Carlos <c.penaa@uniandes.edu.co>
2024-12-03 02:19:59 -0500

    fix bug user?

---
+++ instituciones/instituciones/decorators.py
@@ -0,0 +1,16 @@
+from django.shortcuts import redirect
+from django.contrib.auth import logout
+
+from instituciones.monitoring.auth0backend import getRole
+
+def admin_required(view_func):
+    """
+    Decorator for views that checks that the user is logged in and is an admin,
+    logging out the user if necessary.
+    """
+    def _wrapped_view_func(request, *args, **kwargs):
+        if not request.user.is_authenticated or not getRole(request) == "admin":
+            logout(request)
+            return redirect('/logout')
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view_func
--- instituciones/instituciones/views.py
+++ instituciones/instituciones/views.py
@@ -7,8 +7,11 @@ from .logic.factura_logic import get_facturas, create_factura
 from django.contrib.auth.decorators import login_required
 # Descomentar cuando se cree el archivo monitoring/auth0backend.py
 from monitoring.auth0backend import getRole
+from .decorators import admin_required
+
 
 @login_required
+@admin_required
 def factura_list(request):
     role = getRole(request)
     if role == "admin":
@@ -16,11 +19,13 @@ def factura_list(request):
         context = {
             'facturas_list': facturas
         }
-        return render(request, 'instituciones/factura.html', context)
+        return render(request, 'Factura/factura.html', context)
     else:
+        # Redirect me to /logout when I'm not an admin
         return HttpResponse("Unauthorized User")
 
 @login_required
+@admin_required
 def factura_create(request):
     role = getRole(request)
     if role == "admin":
